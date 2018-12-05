38337de48de8e098ce0ca063a54056b3b7c4aac4
johnalexandergreene <john@fleen.org>
2018-12-04 22:16:59 -0600

    got good full-palette uniform strobe going

---
+++ videoCreationSystems/vcs_test014_like12butslower_4_6_strobe/AudioRenderer0.java
@@ -0,0 +1,13 @@
+package org.fleen.whelmer.videoCreationSystems.vcs_test014_like12butslower_4_6_strobe;
+
+import org.fleen.whelmer.core.AudioRenderer;
+import org.fleen.whelmer.core.Whelmer;
+
+public class AudioRenderer0 implements AudioRenderer{
+
+  public int[] renderAudioFrame(Whelmer whelmer){
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
